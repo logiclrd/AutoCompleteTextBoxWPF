c9b8853466f002fb6e8e904cc57c154e7f438888
Jonathan Gilbert <JonathanG@iQmetrix.com>
2019-02-22 22:07:14 -0600

    Added TestDriver project.

---
+++ TestDriver/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+using AutoCompleteTextBoxWPF;
+
+namespace TestDriver
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			txtTest.ItemProvider = new AutoCompleteProvider();
+		}
+
+		class AutoCompleteProvider : IAutoCompleteItemProvider
+		{
+			public IEnumerable<string> GetItems()
+			{
+				return Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Windows));
+			}
+		}
+	}
+}
